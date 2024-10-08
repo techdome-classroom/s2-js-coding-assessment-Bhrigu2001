299d547386f79ce15b2ba0bf635abf8f4c52ebae
bhrigu <bhriguyadav8210@gmail.com>
2024-10-08 14:19:35 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -4,11 +4,39 @@
  */
 var romanToInt = function(s) {
 
-
-
-
-
+    function romanToInt(s) {
+        // Map of Roman numerals and their integer values
+        const romanMap = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
+        
+        let total = 0;
+        
+        for (let i = 0; i < s.length; i++) {
+            let currentVal = romanMap[s[i]];
+            let nextVal = romanMap[s[i + 1]];
     
+            // If the current value is less than the next value, subtract it
+            if (currentVal < nextVal) {
+                total -= currentVal;
+            } else {
+                // Otherwise, add it
+                total += currentVal;
+            }
+        }
+        
+        return total;
+    }
+
+
+
+
     
 };
 
